0674673c9065bcf6e7830d36bc4216c88efb8f21
Jon.Coello <Jon.Coello@ACCESS-1303SF2>
2017-02-09 16:29:53 +0000

    get hello world enpoint

---
+++ LoggingExample.API/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace LoggingExample.API.Controllers
+{
+    [RoutePrefix("api/hello")]
+    public class HelloController : ApiController
+    {
+        [Route("")]
+        public async Task<object> Get()
+        {
+            return new
+            {
+                Message = "hello world"
+            };
+        }
+
+    }
+}
--- LoggingExample.API/Startup.cs
+++ LoggingExample.API/Startup.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web;
+using System.Web.Http;
 
 namespace LoggingExample.API
 {
     public class Startup
     {
+
+        public void Configuration(IAppBuilder app) {
+
+            var config = new HttpConfiguration();
+
+            // json
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+
+        }
+
     }
 }
