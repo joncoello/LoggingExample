e6e6fb5145573f58f3e5e60ed45a6260c8325313
Jon.Coello <Jon.Coello@ACCESS-1303SF2>
2017-02-09 16:19:59 +0000

    build failing test for hellow world endpoint

---
+++ LoggingExample.APITests/HelloWorldTests.cs
@@ -0,0 +1,41 @@
+﻿using LoggingExample.API;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LoggingExample.APITests
+{
+    [Trait("Category", "API")]
+    public class HelloWorldTests
+    {
+
+        [Fact]
+        public async Task HelloWorld_Get()
+        {
+
+            using (var serverAndClient = new HttpServerAndClient<Startup>())
+            {
+
+                var response = await serverAndClient.Client.GetAsync("api/hello");
+
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                var obj = (JObject)JsonConvert.DeserializeObject(content);
+
+                Assert.Equal("hello world", obj["message"].ToString());
+
+
+            }
+
+        }
+
+
+    }
+}
